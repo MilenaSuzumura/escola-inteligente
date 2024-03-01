1b7a787adf5c36cde975e68d55ce7ae373cb3364
MilenaSuzumura <milena.suzumura11@gmail.com>
2024-03-01 12:22:15 -0300

    tentei fazer a media dos alunos de novo

--- src/main/java/com/trybe/java/escolainteligente/Secretaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Secretaria.java
@@ -8,7 +8,8 @@ public class Secretaria {
    * Método calcularMedia.
    */
   public static double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
-    return (nota1 + nota2 + nota3 + nota4) / 4;
+    double soma = nota1 + nota2 + nota3 + nota4;
+    return soma / 4;
   }
 
   /**
@@ -17,23 +18,23 @@ public class Secretaria {
   public static void coletarInformacoes() {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Qual o nome da Pessoa Estudante?");
+    System.out.print("Qual o nome da Pessoa Estudante?\n");
     String nome = scanner.next();
 
-    System.out.print("Avaliação 1:");
+    System.out.print("Avaliação 1:\n");
     double nota1 = Double.parseDouble(scanner.next());
 
-    System.out.print("Avaliação 2:");
+    System.out.print("Avaliação 2:\n");
     double nota2 = Double.parseDouble(scanner.next());
 
-    System.out.print("Avaliação 3:");
+    System.out.print("Avaliação 3:\n");
     double nota3 = Double.parseDouble(scanner.next());
 
-    System.out.print("Avaliação 4:");
+    System.out.print("Avaliação 4:\n");
     double nota4 = Double.parseDouble(scanner.next());
 
     double media = calcularMedia(nota1, nota2, nota3, nota4);
 
-    System.out.println("A média das notas de " + nome + " é " + media);
+    System.out.println("A média das notas de " + nome + " é " + media + ".");
   }
 }
