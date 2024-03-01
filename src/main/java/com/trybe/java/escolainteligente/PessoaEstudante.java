e29f99b3a91d05c902da082eb4f1f759f394324d
MilenaSuzumura <milena.suzumura11@gmail.com>
2024-03-01 11:31:19 -0300

    Tentei fazer a contagem da idade em dias

--- src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
+++ src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
@@ -8,14 +8,34 @@ public class PessoaEstudante {
    * Método calcularIdadeEmDias.
    */
   public static int calcularIdadeEmDias(int anos, int meses, int dias) {
-    System.out.println("TODO: Implementar a função calcular Idade em Dias.");
-    return 0;
-  }
+    int anosMeses = anos * 12;
+    int mesesTotal = anosMeses + meses;
+    int diasMeses = mesesTotal * 30;
+    int diasTotais = diasMeses + dias;
+    return diasTotais;
+  };
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações da Pessoa Estudante ");
-  }
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Qual o nome da Pessoa Estudante?");
+    String nome = scanner.next();
+
+    System.out.println("Qual a sua idade em anos, meses e dias?");
+    System.out.print("anos:");
+    int anos = Integer.parseInt(scanner.next());
+
+    System.out.print("meses:");
+    int meses = Integer.parseInt(scanner.next());
+
+    System.out.print("dias:");
+    int dias = Integer.parseInt(scanner.next());
+
+    int totalDias = calcularIdadeEmDias(anos, meses, dias);
+
+    System.out.println("A idade de" + nome + "em dias é" + totalDias);
+  };
 }
